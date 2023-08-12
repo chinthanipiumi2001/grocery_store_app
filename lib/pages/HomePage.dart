f33569a513340e1f55e75d9802b4fe2d1732f876
user <dissanayakepiumi75@gmail.com>
2023-08-12 18:52:43 +0530

    update home page

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -4,7 +4,53 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(),
+      body: ListView(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                for(int i=1; i<4; i++)
+              Image.asset(
+                "images/deal$i.jpg",
+                height: 350,
+                fit: BoxFit.cover,
+              ),
+            ],),
+          ),
+          SizedBox(height:20),
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                ),
+              ]
+            ),
+            child: GridView.count(
+              crossAxisCount: 4,
+              childAspectRatio:1 ,
+              crossAxisSpacing: 15,
+              mainAxisSpacing: 15,
+              shrinkWrap: true,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 230, 177),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
