4954c0ed83de2975573c3bea84f7d84b56d43ca4
user <dissanayakepiumi75@gmail.com>
2023-08-12 19:35:09 +0530

    add infinity

--- lib/widgets/ HomePagePosts.dart
+++ lib/widgets/ HomePagePosts.dart
@@ -3,6 +3,27 @@ import 'package:flutter/material.dart';
 class HomePagePosts extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+          ),
+        ],
+      ),
+    );
   }
 }
