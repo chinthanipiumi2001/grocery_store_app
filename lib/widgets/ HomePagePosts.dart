1c9ca3f10bc7204e01fcdc34bc9ad1a588a7cd27
user <dissanayakepiumi75@gmail.com>
2023-08-14 21:10:50 +0530

    add png 1 image and text item name

--- lib/widgets/ HomePagePosts.dart
+++ lib/widgets/ HomePagePosts.dart
@@ -29,6 +29,60 @@ class HomePagePosts extends StatelessWidget{
               ),
             ),
           ),
+          SizedBox(height: 15),
+          GridView.count(
+            physics: NeverScrollableScrollPhysics(),
+            crossAxisCount: 2,
+            childAspectRatio: 0.7,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+            shrinkWrap: true,
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Color(0xFFF7F5F8),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Column(children: [
+                  InkWell(
+                    onTap: (){
+                      //Navigator.pushNamed(context, "itemPage");
+                    },
+                    child: Image.asset(
+                      "images/1.png",
+                      fit: BoxFit.contain,
+                      height: 130,
+                      width: 120,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Item Name",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],),
+              ),
+            ],
+          ),
         ],
       ),
     );
