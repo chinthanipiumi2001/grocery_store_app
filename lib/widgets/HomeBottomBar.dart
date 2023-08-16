834a6d58cccfcded85a88ea57e475fa7ce4695e6
user <dissanayakepiumi75@gmail.com>
2023-08-16 20:55:52 +0530

    apply new items home bottom pg

--- lib/widgets/HomeBottomBar.dart
+++ lib/widgets/HomeBottomBar.dart
@@ -3,6 +3,30 @@ import 'package:flutter/material.dart';
 class HomeBottomBar extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Container(
+      height: 80,
+      padding: EdgeInsets.symmetric(horizontal: 25,vertical: 12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+              Icons.home,
+            color: Color(0xFFF7CA0F),
+            size: 32,
+          ),
+          Text(
+            "Home",
+            style: TextStyle(
+              color: Color(0xFFF7CA0F),
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
