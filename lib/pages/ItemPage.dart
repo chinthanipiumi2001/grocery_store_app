09936ae40660205f0b9c3bb6a4d0248c6115fdd5
user <dissanayakepiumi75@gmail.com>
2023-08-17 17:21:35 +0530

    update item pg

--- lib/pages/ItemPage.dart
+++ lib/pages/ItemPage.dart
@@ -3,6 +3,57 @@ import 'package:flutter/material.dart';
 class ItemPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            color: Color.fromARGB(225, 225, 230, 177),
+            width: 390,
+            padding: EdgeInsets.symmetric(vertical: 15),
+            child: Column(
+              children: [
+                Padding(
+                    padding:EdgeInsets.symmetric(vertical: 15,horizontal: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: (){
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.arrow_back,
+                          size: 28,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                            ),
+                          ]
+                        ),
+                        child: Icon(
+                          Icons.favorite,
+                          size: 30,
+                          color:Color(0xFFFFB608),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
