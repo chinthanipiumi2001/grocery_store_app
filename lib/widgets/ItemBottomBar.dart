5ff951f08454ad8e380c48b0d8b724f49455297e
user <dissanayakepiumi75@gmail.com>
2023-08-20 10:23:12 +0530

    add GestureDetector

--- lib/widgets/ItemBottomBar.dart
+++ lib/widgets/ItemBottomBar.dart
@@ -3,6 +3,28 @@ import 'package:flutter/material.dart';
 class ItemBottomBar extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Container(
+      height: 80,
+      padding: EdgeInsets.symmetric(vertical: 12,horizontal: 25),
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GestureDetector(
+            onTap: (){},
+            child: Container(
+              height: 60,
+              width: 80,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
