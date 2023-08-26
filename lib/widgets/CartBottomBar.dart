f521eb0febb81ae77a904da974811a0dbda0dba8
user <dissanayakepiumi75@gmail.com>
2023-08-26 18:40:56 +0530

    add spread radius and blur radius

--- lib/widgets/CartBottomBar.dart
+++ lib/widgets/CartBottomBar.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class CartBottomBar extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Container(
+      height: 130,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Color(0xFFF5F9FD),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 1,
+            blurRadius: 5,
+          ),
+        ],
+      ),
+    );
   }
 }
