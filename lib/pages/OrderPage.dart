0f649fde9ec7e6ad84399664c2ab13f119621694
user <dissanayakepiumi75@gmail.com>
2023-08-28 23:50:18 +0530

    crate form order now

--- lib/pages/OrderPage.dart
+++ lib/pages/OrderPage.dart
@@ -34,6 +34,139 @@ class OrderPage extends StatelessWidget{
                   ),
                 ),
               ),
+              Container(
+                margin: EdgeInsets.only(left: 15,top: 20),
+                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "First Name",
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 15,top: 20),
+                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Last Name",
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 15,top: 20),
+                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Mobile Number",
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 15,top: 20),
+                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Email",
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 15,top: 20),
+                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Address",
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 15,top: 20),
+                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "city",
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 15,top: 20),
+                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Postal/Zip code",
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 15,horizontal: 20),
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFFB608),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    "Order Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ],
